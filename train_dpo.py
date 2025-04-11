aef2e57193b96e1a74a7d92ea677cb4f0160c7a8
ryotaro <kakuda40611@gmail.com>
2025-04-11 21:32:33 +0800

    Bug fix: point cloud was not correctly passed to the model

--- train_dpo.py
+++ train_dpo.py
@@ -6,8 +6,6 @@ from typing import Dict, List, Literal, Optional, Tuple, Union
 
 import cadquery as cq
 import numpy as np
-import open3d
-import skimage.io
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -22,7 +20,7 @@ from transformers.models.qwen2.modeling_qwen2 import Qwen2ForCausalLM
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 from trl import DPOConfig, DPOTrainer
-from trl.trainer.utils import pad
+from trl.trainer.utils import flush_left, pad, pad_to_length, selective_log_softmax
 
 
 ######## CONFIG ########
@@ -89,6 +87,21 @@ class CADRecode(Qwen2ForCausalLM):
         return_dict: Optional[bool] = None,
         cache_position: Optional[torch.LongTensor] = None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
+
+        # print("################")
+        # print(f"[Model] input_ids: {input_ids.shape if isinstance(input_ids, torch.Tensor) else input_ids}")
+        # print(f"[Model] attention_mask: {attention_mask.shape if isinstance(attention_mask, torch.Tensor) else attention_mask}")
+        # print(f"[Model] point_cloud: {point_cloud.shape if isinstance(point_cloud, torch.Tensor) else point_cloud}")
+        # print(f"[Model] position_ids: {position_ids.shape if isinstance(position_ids, torch.Tensor) else position_ids}")
+        # print(f"[Model] past_key_values: {past_key_values if isinstance(past_key_values, torch.Tensor) else past_key_values}")
+        # print(f"[Model] inputs_embeds: {inputs_embeds if isinstance(inputs_embeds, torch.Tensor) else inputs_embeds}")
+        # print(f"[Model] labels: {labels.shape if isinstance(labels, torch.Tensor) else labels}")
+        # print(f"[Model] use_cache: {use_cache}")
+        # print(f"[Model] output_attentions: {output_attentions}")
+        # print(f"[Model] output_hidden_states: {output_hidden_states}")
+        # print(f"[Model] return_dict: {return_dict}")
+        # print(f"[Model] cache_position: {cache_position.shape if isinstance(cache_position, torch.Tensor) else cache_position}")
+        # print("################")
 
         # Determine if this is the first forward pass (no past_key_values)
         # and if we need to process point clouds based on input args
@@ -363,6 +376,232 @@ class DataCollatorForDPOCADRecode:
         if point_clouds_tensor.shape != (batch_size, self.num_point_tokens, 3): # Assuming 3D points
              raise ValueError(f"Unexpected point_cloud tensor shape: {point_clouds_tensor.shape}. Expected: {(batch_size, self.num_point_tokens, 3)}")
 
+        output["point_cloud"] = point_clouds_tensor
+
+        # for k, v in output.items():
+        #     print(f"[DataCollator] output[{k}] = {v.shape if isinstance(v, torch.Tensor) else v}")
+
+        return output
+
+
+######## TRAINER ########
+
+class CADRecodeDPOTrainer(DPOTrainer):
+
+    @staticmethod
+    def concatenated_inputs(
+            batch: dict[str, Union[list, torch.LongTensor]], padding_value: int,
+        ) -> dict[str, torch.LongTensor]:
+        output = {}
+
+        # For the prompt, the input_ids are the same for both the chosen and rejected responses
+        output["prompt_input_ids"] = torch.cat([batch["prompt_input_ids"], batch["prompt_input_ids"]], dim=0)
+        output["prompt_attention_mask"] = torch.cat(
+            [batch["prompt_attention_mask"], batch["prompt_attention_mask"]], dim=0
+        )
+        if "pixel_values" in batch:
+            output["pixel_values"] = torch.cat([batch["pixel_values"], batch["pixel_values"]], dim=0)
+
+        if "pixel_attention_mask" in batch:
+            output["pixel_attention_mask"] = torch.cat(
+                [batch["pixel_attention_mask"], batch["pixel_attention_mask"]], dim=0
+            )
+        if "image_sizes" in batch:
+            output["image_sizes"] = torch.cat([batch["image_sizes"], batch["image_sizes"]], dim=0)
+
+        # Concatenate the chosen and rejected completions
+        max_completion_length = max(batch["chosen_input_ids"].shape[1], batch["rejected_input_ids"].shape[1])
+        output["completion_input_ids"] = torch.cat(
+            (
+                pad_to_length(batch["chosen_input_ids"], max_completion_length, pad_value=padding_value),
+                pad_to_length(batch["rejected_input_ids"], max_completion_length, pad_value=padding_value),
+            ),
+        )
+        output["completion_attention_mask"] = torch.cat(
+            (
+                pad_to_length(batch["chosen_attention_mask"], max_completion_length, pad_value=0),
+                pad_to_length(batch["rejected_attention_mask"], max_completion_length, pad_value=0),
+            ),
+        )
+
+        # add point cloud
+        output["point_cloud"] = torch.cat([batch["point_cloud"], batch["point_cloud"]], dim=0)
+
+        return output
+
+    def concatenated_forward(self, model: nn.Module, batch: dict[str, Union[list, torch.LongTensor]]):
+        """Run the given model on the given batch of inputs, concatenating the chosen and rejected inputs together.
+
+        We do this to avoid doing two forward passes, because it's faster for FSDP.
+        """
+        num_examples = batch["prompt_input_ids"].shape[0]
+
+        concatenated_batch = self.concatenated_inputs(batch, padding_value=self.padding_value)
+
+        model_kwargs = {"point_cloud": concatenated_batch["point_cloud"]}  # NOTE: CHANGED HERE!!!
+        if self.aux_loss_enabled:
+            model_kwargs["output_router_logits"] = True
+
+        # Add the pixel values and attention masks for vision models
+        if "pixel_values" in concatenated_batch:
+            model_kwargs["pixel_values"] = concatenated_batch["pixel_values"]
+        if "pixel_attention_mask" in concatenated_batch:
+            model_kwargs["pixel_attention_mask"] = concatenated_batch["pixel_attention_mask"]
+        if "image_sizes" in concatenated_batch:
+            model_kwargs["image_sizes"] = concatenated_batch["image_sizes"]
+
+        prompt_input_ids = concatenated_batch["prompt_input_ids"]
+        prompt_attention_mask = concatenated_batch["prompt_attention_mask"]
+        completion_input_ids = concatenated_batch["completion_input_ids"]
+        completion_attention_mask = concatenated_batch["completion_attention_mask"]
+        if self.is_encoder_decoder:
+            labels = completion_input_ids
+            labels[completion_attention_mask == 0] = self.label_pad_token_id
+            outputs = model(
+                input_ids=prompt_input_ids,
+                attention_mask=prompt_attention_mask,
+                labels=labels,  # we need the labels for the logits to be returned
+                **model_kwargs,
+            )
+            logits = outputs.logits
+            loss_mask = completion_attention_mask.bool()
+        else:
+            # Concatenate the prompt and completion inputs
+            input_ids = torch.cat((prompt_input_ids, completion_input_ids), dim=1)
+            attention_mask = torch.cat((prompt_attention_mask, completion_attention_mask), dim=1)
+            # Mask the prompt but not the completion for the loss
+            loss_mask = torch.cat(
+                (torch.zeros_like(prompt_attention_mask), completion_attention_mask),
+                dim=1,
+            )
+
+            # Flush left to reduce the memory usage
+            # [[0, 0, x, x, x, x],  ->  [[x, x, x, x],
+            #  [0, x, x, x, 0, 0]]       [x, x, x, 0]]
+            attention_mask, input_ids, loss_mask = flush_left(attention_mask, input_ids, loss_mask)
+
+            # Truncate right
+            if self.max_length is not None:
+                if self.truncation_mode == "keep_end":
+                    input_ids = input_ids[:, -self.max_length :]
+                    attention_mask = attention_mask[:, -self.max_length :]
+                    loss_mask = loss_mask[:, -self.max_length :]
+                elif self.truncation_mode == "keep_start":
+                    input_ids = input_ids[:, : self.max_length]
+                    attention_mask = attention_mask[:, : self.max_length]
+                    loss_mask = loss_mask[:, : self.max_length]
+                else:
+                    raise ValueError(
+                        f"Unknown truncation mode: '{self.truncation_mode}'. Should be one of ['keep_end', "
+                        "'keep_start']."
+                    )
+
+            if self.use_logits_to_keep:
+                # Compute logits_to_keep based on loss_mask pattern:
+                # [[0, 0, 0, x, x, x, x],
+                #  [0, 0, 0, x, x, x, 0]]
+                #         ^ start computing logits from here ([:, -(7-3+1):])
+                first_compute_index = loss_mask.nonzero(as_tuple=True)[1].min()
+                logits_to_keep = (loss_mask.shape[1] - first_compute_index).item() + 1  # +1 for the first label
+                model_kwargs["logits_to_keep"] = logits_to_keep
+
+            if self.padding_free:
+                # Flatten the input_ids, position_ids, and loss_mask
+                # input_ids = [[a, b, c, 0], ->     input_ids = [[a, b, c, d, e, f, g]]
+                #              [d, e, f, g]]     position_ids = [[0, 1, 2, 0, 1, 2, 3]]
+                input_ids = input_ids[attention_mask.bool()].unsqueeze(0)
+                loss_mask = loss_mask[attention_mask.bool()].unsqueeze(0)
+                position_ids = attention_mask.cumsum(1)[attention_mask.bool()].unsqueeze(0) - 1
+                model_kwargs["position_ids"] = position_ids
+            else:
+                model_kwargs["attention_mask"] = attention_mask
+            outputs = model(input_ids, **model_kwargs)
+            logits = outputs.logits
+
+            # Offset the logits by one to align with the labels
+            labels = torch.roll(input_ids, shifts=-1, dims=1)
+            loss_mask = torch.roll(loss_mask, shifts=-1, dims=1).bool()
+
+            if self.use_logits_to_keep:
+                # Align labels with logits
+                # logits:    -,  -, [x2, x3, x4, x5, x6]
+                #                     ^ --------- ^       after logits[:, :-1, :]
+                # labels:   [y0, y1, y2, y3, y4, y5, y6]
+                #                         ^ --------- ^   with logits_to_keep=4, [:, -4:]
+                # loss_mask: [0,  0,  0,  1,  1,  1,  1]
+                labels = labels[:, -logits_to_keep:]
+                loss_mask = loss_mask[:, -logits_to_keep:]
+
+        if logits.shape[:2] != labels.shape[:2]:
+            # for llava, the returned logits include the image tokens (placed before the text tokens)
+            seq_len = labels.shape[1]
+            logits = logits[:, -seq_len:]
+
+        # Compute the log probabilities of the labels
+        labels[~loss_mask] = 0  # dummy token; we'll ignore the losses on these tokens later
+        per_token_logps = selective_log_softmax(logits, labels)
+        per_token_logps[~loss_mask] = 0
+        per_token_logps = torch.roll(per_token_logps, shifts=1, dims=1)
+
+        if self.padding_free:
+            # Unflatten the per_token_logps (shape: [1, sum_seq_len] -> [batch_size, seq_len])
+            batch_size, seq_len = attention_mask.shape
+            per_token_logps_ = torch.zeros(
+                batch_size, seq_len, device=outputs.logits.device, dtype=outputs.logits.dtype
+            )
+            per_token_logps_[attention_mask.bool()] = per_token_logps
+            per_token_logps = per_token_logps_
+
+        all_logps = per_token_logps.sum(-1)
+
+        output = {}
+
+        if self.use_weighting:
+            with torch.no_grad():
+                # Eq (2) of the WPO paper: https://huggingface.co/papers/2406.11827
+                logprobs = F.log_softmax(logits, dim=-1)
+                weights_adjustment_factor = torch.logsumexp(2 * logprobs, dim=-1)  # same as sum(probs**2) in log space
+                per_token_logps_adjusted = per_token_logps - weights_adjustment_factor
+                all_weights = (per_token_logps_adjusted * loss_mask).sum(-1) / loss_mask.sum(-1)
+                chosen_weights = all_weights[:num_examples]
+                rejected_weights = all_weights[num_examples:]
+                output["policy_weights"] = torch.clamp(torch.exp(chosen_weights + rejected_weights), max=1)
+
+        if self.args.rpo_alpha is not None:
+            # Only use the chosen logits for the RPO loss
+            chosen_logits = logits[:num_examples]
+            chosen_labels = labels[:num_examples]
+
+            # Compute the log probabilities of the labels
+            output["nll_loss"] = F.cross_entropy(
+                torch.flatten(chosen_logits, end_dim=1), torch.flatten(chosen_labels, end_dim=1), ignore_index=0
+            )
+
+        if self.loss_type == "ipo":
+            all_logps = all_logps / loss_mask.sum(-1)
+
+        output["chosen_logps"] = all_logps[:num_examples]
+        output["rejected_logps"] = all_logps[num_examples:]
+
+        # Compute the mean logits
+        if self.padding_free:
+            # position_ids contains a sequence of range identifiers (e.g., [[0, 1, 2, 0, 1, 2, 3, ...]]).
+            # There are 2*num_examples ranges in total: the first half corresponds to the chosen tokens,
+            # and the second half to the rejected tokens.
+            # To find the start of the rejected tokens, we look for the num_examples+1-th zero in pos_id.
+            split_idx = (position_ids == 0).nonzero(as_tuple=True)[1][num_examples]
+            mean_chosen_logits = logits[0, :split_idx][loss_mask[0, :split_idx]].mean()
+            mean_rejected_logits = logits[0, split_idx:][loss_mask[0, split_idx:]].mean()
+        else:
+            mean_chosen_logits = logits[:num_examples][loss_mask[:num_examples]].mean()
+            mean_rejected_logits = logits[num_examples:][loss_mask[num_examples:]].mean()
+
+        output["mean_chosen_logits"] = mean_chosen_logits
+        output["mean_rejected_logits"] = mean_rejected_logits
+
+        if self.aux_loss_enabled:
+            output["aux_loss"] = outputs.aux_loss
+
         return output
 
 
@@ -394,7 +633,6 @@ if __name__ == "__main__":
         torch_dtype=torch.bfloat16, # Load in compute dtype
         attn_implementation="flash_attention_2" if torch.cuda.is_available() else None,
         device_map="auto", # Automatically distribute model layers
-        trust_remote_code=True
     )
     # DPOTrainer requires a reference model for KL divergence calculation.
     # If not provided, it creates a copy internally. For QLoRA, providing
@@ -407,7 +645,7 @@ if __name__ == "__main__":
     # Prepare model for K-bit training (important for QLoRA)
     model = prepare_model_for_kbit_training(model)
     # Enable gradient checkpointing in the base model
-    model.gradient_checkpointing_enable()
+    #model.gradient_checkpointing_enable()
 
     # PEFT Config (LoRA)
     peft_config = LoraConfig(
@@ -492,13 +730,13 @@ if __name__ == "__main__":
         weight_decay=0.01, # Added weight decay
         push_to_hub=False, # Set to True to push checkpoints to HF Hub
         report_to="wandb", #"tensorboard"
-        # gradient_checkpointing_kwargs={"use_reentrant": False},
+        gradient_checkpointing_kwargs={"use_reentrant": False},
         # dataset_text_field="", # Not needed with custom collator handling everything
         # dataset_kwargs={"skip_prepare_dataset": True}, # Crucial
         remove_unused_columns=False, # Important: Keep point_cloud column
     )
 
-    trainer = DPOTrainer(
+    trainer = CADRecodeDPOTrainer(
         model=model,
         ref_model=model_ref,  # Let trainer handle creation if None
         args=training_args,
